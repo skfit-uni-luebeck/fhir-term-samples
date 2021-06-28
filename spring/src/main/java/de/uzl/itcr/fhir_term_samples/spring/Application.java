87a5c491503fa69d9ef226575c73bc938ccbf48e
Joshua Wiedekopf <j.wiedekopf@uni-luebeck.de>
2021-06-28 15:05:23 +0000

    add empty spring project

---
+++ spring/src/main/java/de/uzl/itcr/fhir_term_samples/spring/Application.java
@@ -0,0 +1,13 @@
+package de.uzl.itcr.fhir_term_samples.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ spring/src/test/java/de/uzl/itcr/fhir_term_samples/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uzl.itcr.fhir_term_samples.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
