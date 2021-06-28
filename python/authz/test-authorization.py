64f3bc990f4ffcbc11a06bfd5cfbaa61e789f8c9
Joshua Wiedekopf <j.wiedekopf@uni-luebeck.de>
2021-06-28 15:01:37 +0000

    python authorization docs

--- python/authz/test-authorization.py
+++ python/authz/test-authorization.py
@@ -3,14 +3,15 @@ from fhir.resources.capabilitystatement import CapabilityStatement
 from rich import print
 
 def url(endpoint, path):
+    """append the provided path to the endpoint to build an url"""
     return f"{endpoint.rstrip('/')}/{path}"
 
 endpoint = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir/"
-sess = requests.Session()
-cert_file = "../joshua_dfn.pem"
-#cert_file = ("../joshua_dfn.crt", "../joshua_dfn.key")
-sess.cert = cert_file
-response = sess.get(url(endpoint, "metadata"))
-conformance = CapabilityStatement(**response.json())
-software = conformance.software
+sess = requests.Session() # create a persistent session/connection
+cert_file = "../joshua_dfn.pem" # contains both certifacate and private key
+#cert_file = ("../joshua_dfn.crt", "../joshua_dfn.key") # two files for public/private key
+sess.cert = cert_file # applies to all requests initiated from this session
+response = sess.get(url(endpoint, "metadata")) # issue the actual request (without preparing it)
+conformance = CapabilityStatement(**response.json()) # parse as FHIR
+software = conformance.software # access structure of FHIR resource
 print(f"{software.name} version {software.version}")
