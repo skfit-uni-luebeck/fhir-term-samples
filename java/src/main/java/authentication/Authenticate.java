9a8dce5b4c35d45bf1ca467f8fcdc5c793e56af5
Joshua Wiedekopf <j.wiedekopf@uni-luebeck.de>
2021-06-29 16:17:53 +0200

    spring authentication sample, metadata query

--- java/src/main/java/authentication/Authenticate.java
+++ java/src/main/java/authentication/Authenticate.java
@@ -25,13 +25,14 @@ public class Authenticate {
         try {
             SSLContext sslContext = SSLContexts.custom() //we need to load the material from the keystore
                     .loadKeyMaterial(keystorePath, password, password)
-                    .build(); //trust material is added automatically
+                    .build(); //trust material is added automatically from the system default
             CloseableHttpClient client = HttpClients.custom() //configure a HTTP client with the custom SSL context
                     .setSSLContext(sslContext)
                     .build();
             final FhirContext fhirContext = FhirContext.forR4(); //do this exactly once for the lifetime of the app!
             final IRestfulClientFactory restfulClientFactory = fhirContext.getRestfulClientFactory();
             restfulClientFactory.setHttpClient(client); //configure the client factory to use our HTTP client
+            
             final String serverBase = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir";
             IGenericClient fhirClient = fhirContext.newRestfulGenericClient(serverBase); //this will use our HTTP client
             CapabilityStatement capabilities = fhirClient.capabilities().ofType(CapabilityStatement.class).execute();
