4f3ff801ab2cedec6b124fc4f6694cc5e34b21a8
Joshua Wiedekopf <jpwiedekopf@gmail.com>
2021-07-04 11:19:25 +0200

    create auth/FHIR wrapper

--- java/src/main/java/authentication/Authenticate.java
+++ java/src/main/java/authentication/Authenticate.java
@@ -1,44 +1,25 @@
 package authentication;
 
-import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.hl7.fhir.r4.model.CapabilityStatement;
 
-import javax.net.ssl.SSLContext;
 import java.io.IOException;
-import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import fhirwrapper.FhirClientWrapper;
 
 public class Authenticate {
 
     public static void main(String[] args) {
-        URL keystorePath = ClassLoader.getSystemResource("keystore.jks"); //get keystore from src/main/resources
-        char[] password = "pw".toCharArray(); //password of keystore and key within keystore (can be different)
         try {
-            SSLContext sslContext = SSLContexts.custom() //we need to load the material from the keystore
-                    .loadKeyMaterial(keystorePath, password, password)
-                    .build(); //trust material is added automatically from the system default
-            CloseableHttpClient client = HttpClients.custom() //configure a HTTP client with the custom SSL context
-                    .setSSLContext(sslContext)
-                    .build();
-            final FhirContext fhirContext = FhirContext.forR4(); //do this exactly once for the lifetime of the app!
-            final IRestfulClientFactory restfulClientFactory = fhirContext.getRestfulClientFactory();
-            restfulClientFactory.setHttpClient(client); //configure the client factory to use our HTTP client
-            
-            final String serverBase = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir";
-            IGenericClient fhirClient = fhirContext.newRestfulGenericClient(serverBase); //this will use our HTTP client
+            IGenericClient fhirClient = FhirClientWrapper.getFhirClient(null, null, null);
             CapabilityStatement capabilities = fhirClient.capabilities().ofType(CapabilityStatement.class).execute();
             //query the metadata from the TS and parse the software component
             System.out.printf("%s %s%n", capabilities.getSoftware().getName(), capabilities.getSoftware().getVersion());
-        } catch (NoSuchAlgorithmException | KeyStoreException | UnrecoverableKeyException | CertificateException | IOException | KeyManagementException e) {
+        } catch (UnrecoverableKeyException | CertificateException | NoSuchAlgorithmException | KeyStoreException | IOException | KeyManagementException e) {
             e.printStackTrace();
         }
     }
---
+++ java/src/main/java/fhirwrapper/FhirClientWrapper.java
@@ -0,0 +1,39 @@
+package fhirwrapper;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.api.IRestfulClient;
+import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+public class FhirClientWrapper {
+    final static FhirContext fhirContext = FhirContext.forR4();
+
+    public static IGenericClient getFhirClient(String serverBase, String keystoreFilename, String keystorePassword) throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
+        if (keystoreFilename == null) keystoreFilename = "keystore.jks";
+        if (keystorePassword == null) keystorePassword = "pw";
+        URL keystorePath = ClassLoader.getSystemResource(keystoreFilename); //get keystore from src/main/resources
+        char[] password = keystorePassword.toCharArray(); //password of keystore and key within keystore (can be different)
+        SSLContext sslContext = SSLContexts.custom() //we need to load the material from the keystore
+                .loadKeyMaterial(keystorePath, password, password)
+                .build(); //trust material is added automatically from the system default
+        CloseableHttpClient client = HttpClients.custom() //configure a HTTP client with the custom SSL context
+                .setSSLContext(sslContext)
+                .build();
+        final IRestfulClientFactory restfulClientFactory = fhirContext.getRestfulClientFactory();
+        restfulClientFactory.setHttpClient(client); //configure the client factory to use our HTTP client
+        if (serverBase == null) serverBase = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir";
+        return fhirContext.newRestfulGenericClient(serverBase);
+    }
+}
