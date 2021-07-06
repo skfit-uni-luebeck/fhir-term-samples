cb1db89ffd67e629fbcda21041adbad1cde95203
Joshua Wiedekopf <j.wiedekopf@uni-luebeck.de>
2021-07-06 17:05:03 +0200

    find_matches WIP

---
+++ python/find_matches.py
@@ -0,0 +1,70 @@
+from get_session import (
+    FhirApi,
+    NotEmptyValidator,
+    ValueListValidator,
+    ParametersValueTypeValidator,
+)
+from rich import print
+from fhir.resources.bundle import Bundle
+from fhir.resources.parameters import Parameters, ParametersParameter
+from fhir.resources.codesystem import CodeSystem
+from typing import List
+import questionary
+
+
+fhir_api = FhirApi()
+cs_bundle: Bundle = fhir_api.request_and_parse_fhir("CodeSystem", Bundle)
+cs_list: List[CodeSystem] = [r.resource for r in cs_bundle.entry]
+cs_options = [
+    questionary.Choice(f"{r.url} {r.version}", value=(r.url, r.version))
+    for r in cs_list
+]
+cs_options.sort(key=lambda c: c.value)
+
+sel_cs, sel_cs_version = questionary.select("CodeSystem", choices=cs_options).ask()
+print(sel_cs, sel_cs_version)
+
+
+add_more_prop = True
+parameter_properties = []
+while add_more_prop:
+    code = questionary.text("Code for property?", validate=NotEmptyValidator).ask()
+    valueChoices = [
+        "valueCode",
+        "valueCoding",
+        "valueString",
+        "valueInteger",
+        "valueBoolean",
+        "valueDateTime",
+    ]
+    valueType = questionary.autocomplete(
+        "Value type?", choices=valueChoices, validate=ValueListValidator(valueChoices)
+    ).ask()
+    if valueType == "valueBoolean":
+        value = questionary.confirm("Is the value True?").ask()
+    else:
+        value = questionary.text(
+            "Value of the property?", validate=ParametersValueTypeValidator(valueType)
+        ).ask()
+    parameter_properties.append(
+        ParametersParameter(
+            **{
+                "name": "property",
+                "part": [
+                    ParametersParameter(**{"name": "code", "valueCode": code}),
+                    ParametersParameter(**{"name": "value", valueType: value}),
+                ],
+            }
+        )
+    )
+    request_parameters = Parameters(
+        **{
+            "parameter": [
+                {"name": "system", "valueUri": sel_cs},
+                {"name": "version", "valueString": sel_cs_version},
+            ]
+            + parameter_properties
+        }
+    )
+    print(request_parameters.json())
+    add_more_prop = questionary.confirm("Continue adding properties?").ask()
--- python/get_session.py
+++ python/get_session.py
@@ -1,22 +1,86 @@
 from requests import Session
-from rich import print
+from rich import print, inspect
 from fhir.resources.bundle import Bundle
 from fhir.resources.parameters import Parameters, ParametersParameter
-from typing import Optional, Tuple
+from fhir.resources.fhirtypes import DateTime, Code
+from fhir.resources.coding import Coding
+from typing import Optional, Tuple, List
 from urllib.request import getproxies
+import questionary
+import pydantic
+from pydantic.error_wrappers import ValidationError as PydanticError
+from questionary import Validator, ValidationError
+
+
+class NotEmptyValidator(Validator):
+    "validate that the value in non-empty"
+
+    def validate(self, document):
+        if len(document.text) == 0:
+            raise ValidationError(
+                message="Please enter a value",
+                cursor_position=len(document.text),
+            )
+
+
+class ValueListValidator(Validator):
+    "validate that the value is from the provided list of permitted codes"
+
+    allowed_choices: List[str]
+
+    def __init__(self, choices: List[str]):
+        self.allowed_choices = choices
+
+    def validate(self, document):
+        if document.text not in self.allowed_choices:
+            raise ValidationError(
+                message=f"Please enter one of: {'|'.join(self.allowed_choices)}",
+                cursor_position=len(document.text),
+            )
+
+
+class ParametersValueTypeValidator(Validator):
+    "validate that a parameter is a valid instance of the provided FHIR type"
+
+    fhir_type: str
+
+    def __init__(self, fhir_type: str):
+        self.fhir_type = fhir_type
+
+    def validate(self, document):
+        def error(message):
+            raise ValidationError(message=message, cursor_position=len(document.text))
+
+        def empty_error():
+            error("Please enter a value")
+
+        if len(document.text) == 0:
+            empty_error()
+
+        try:
+            p = ParametersParameter(**{
+                "name": "test",
+                self.fhir_type: document.text
+                }) # abuse ParametersParameter to validate the data for us...
+        except PydanticError as e:
+            messages = " | ".join([q["msg"] for q in e.errors()])
+            #inspect(messages)
+            error(f"{len(e.errors())} validation error(s): {messages}")
+
 
 class FhirApi:
     "encapsulate a connection to a FHIR TS"
-    session : Session # hold the requests session
-    cert_file: str # path to the certificate
-    endpoint: str # the endpoint of the FHIR TS
-    print_url: bool # if true, request URLs will be printed
+    session: Session  # hold the requests session
+    cert_file: str  # path to the certificate
+    endpoint: str  # the endpoint of the FHIR TS
+    print_url: bool  # if true, request URLs will be printed
 
     def __init__(
-            self, 
-            cert_file: Optional[str] = "dfn.pem", 
-            endpoint: str = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir", 
-            print_url: bool = True):
+        self,
+        cert_file: Optional[str] = "dfn.pem",
+        endpoint: str = "https://terminology-highmed.medic.medfak.uni-koeln.de/fhir",
+        print_url: bool = True,
+    ):
         self.session = Session()
         self.cert_file = cert_file
         # set proxy from environment (HTTP_PROXY/HTTPS_PROXY) if set
@@ -24,12 +88,14 @@ class FhirApi:
             self.session.proxies = getproxies()
             print(f"Using proxies: {getproxies()}")
         self.session.cert = self.cert_file
-        self.endpoint = endpoint.rstrip("/") # remove slash at end to make sure that joining works
+        self.endpoint = endpoint.rstrip(
+            "/"
+        )  # remove slash at end to make sure that joining works
         self.print_url = print_url
-    
+
     def build_url(self, path: str) -> str:
-        return self.endpoint + "/" + path.lstrip("/") # remove slash at beginning also
-    
+        return self.endpoint + "/" + path.lstrip("/")  # remove slash at beginning also
+
     def request_from_url_parse_fhir(self, url: str, resource):
         """
         request from an absolute URL, and parse the result as the given FHIR resource.
@@ -41,15 +107,17 @@ class FhirApi:
             print(f"Requesting from {url}")
         response = self.session.get(url)
         if response.status_code >= 200 and response.status_code < 300:
-            try: # very simplistic error handling
+            try:  # very simplistic error handling
                 j = response.json()
                 if j["resourceType"] == "ValueSet" and "status" not in j:
-                   j["status"] = "unknown" 
-                return resource(**j) # parse with given class
+                    j["status"] = "unknown"
+                return resource(**j)  # parse with given class
             except Exception as e:
                 raise ValueError(f"Parsing the response was not possible") from e
         else:
-            raise SystemError(f"Error requesting from {url}, status code {response.status_code}")
+            raise SystemError(
+                f"Error requesting from {url}, status code {response.status_code}"
+            )
 
     def request_and_parse_fhir(self, path: str, resource):
         """
@@ -63,7 +131,9 @@ class FhirApi:
         "return a parameter from the Parameters object, by the name, or None if not found"
         return next((p for p in parameters.parameter if p.name == name), None)
 
-    def lookup_code_display(self, url: str, code: str, version: Optional[str] = None) -> Tuple[bool, Optional[str]]:
+    def lookup_code_display(
+        self, url: str, code: str, version: Optional[str] = None
+    ) -> Tuple[bool, Optional[str]]:
         """
         execute a validate-code request for the given (url, code). This operation validates
         that the concept is valid within the CodeSystem, and returns the display as a bonus.
@@ -88,10 +158,11 @@ class FhirApi:
     def request_bundle(self, path: str) -> Bundle:
         """
         request a bundle from the provided path. This method is syntactic sugar, to take advantage
-        of type hints for Bundle requests, which are the most common requests in this demontration 
+        of type hints for Bundle requests, which are the most common requests in this demontration
         package.
         """
         return self.request_and_parse_fhir(path, Bundle)
+
 
 if __name__ == "__main__":
     print("This script is not meant for running interactively.")
